1b5234ef7fd96d4d64342326c912cc32f44bdf27
felixtong <felix@51qudian.com>
2026-01-02 12:23:16 +0800

    ratelimit

--- src/main/java/xw/szbz/cn/repository/JiTuRepository.java
+++ src/main/java/xw/szbz/cn/repository/JiTuRepository.java
@@ -1,6 +1,8 @@
 package xw.szbz.cn.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import xw.szbz.cn.entity.JiTu;
 
@@ -27,4 +29,16 @@ public interface JiTuRepository extends JpaRepository<JiTu, Long> {
     Optional<JiTu> findFirstByGenderAndYearAndMonthAndDayAndHourOrderByCreateTimeDesc(
         String gender, Integer year, Integer month, Integer day, Integer hour
     );
+    
+    /**
+     * 查询指定openId在指定时间范围内的提交次数
+     * @param openId 用户openId
+     * @param startTime 开始时间（13位时间戳）
+     * @param endTime 结束时间（13位时间戳）
+     * @return 提交次数
+     */
+    @Query("SELECT COUNT(j) FROM JiTu j WHERE j.openId = :openId AND j.createTime >= :startTime AND j.createTime <= :endTime")
+    long countByOpenIdAndCreateTimeBetween(@Param("openId") String openId, 
+                                           @Param("startTime") Long startTime, 
+                                           @Param("endTime") Long endTime);
 }
